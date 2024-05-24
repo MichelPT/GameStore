45e14e0c36519fd2f14851fd60a35383bea28e75
MichelPT <michelpt76767@gmail.com>
2024-05-24 15:13:43 +0700

    added services, configs

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const cors = require("cors");
+const pcRouter = require("./pc");
+const port = "3100";
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use("/pc", pcRouter);
+
+app.get("/", (req, res) => {
+  res.send("Welcome to PC Service! 😁");
+});
+
+app.listen(port, () => {
+  console.log("Server Connected on PORT: " + port + "/");
+});
