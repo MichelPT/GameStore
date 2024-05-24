8cf7b459db9af48f80dcc5bb3c9950c12d7d7ac9
MichelPT <michelpt76767@gmail.com>
2024-05-24 15:08:48 +0700

    added services and configs

---
+++ app.js
@@ -0,0 +1,21 @@
+// Mengimport package
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const port = 3100;
+const ps4Router = require("./ps4");
+
+// Supaya API dapat diakses di domain yang berbeda
+app.use(cors());
+
+// Buat ngubah request body yang berupa json ke dalam object
+app.use(express.json());
+
+app.use("/ps4", ps4Router);
+
+app.get("/", (req, res) => {
+  res.send("Hello from ps4-service! 😁");
+});
+
+// Menjalankan server di port 3001
+app.listen(port, () => console.log("Server terkoneksi pada port " + port));
